68734ecc8558190f7c9b27e59b1a25f4f9ab324a
Rahma <rahma.muhammed1999@gmail.com>
2023-02-25 14:08:43 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form</title>
+    <link rel="stylesheet" href="index.css">
+
+</head>
+
+<body>
+    <div class="Form_Main_Container">
+        <div class="Sub1_Form_Container">
+            <h1 class="Txt_Login">Welcome</h1>
+        </div>
+        <div class="Sub2_Form_Container">
+            <div class="Inp_Email_Container">
+                <input class="Inp_Email" type="email" name="" id="" placeholder="Email">
+            </div>
+            <div class="Inp_Password_Container">
+                <input class="Inp_Password" type="password" name="" id="" placeholder="Password">
+            </div>
+            <div class="Remember_Checkbox_Container">
+                <div class="Lbl_Remeber_Txt_Container">
+                    <label class="Lbl_Remeber_Txt" for="">Remeber me</label>
+                </div>
+                <div class="Lbl_Remember_Checkbox_Container">
+                    <input class="Remember_Checkbox" type="checkbox" name="" id="">
+                </div>
+            </div>
+            <div class="But_Login_Container">
+                <button class="But_Login">Login</button>
+            </div>
+        </div>
+    </div>
+    <script>
+    </script>
+</body>
+
+</html>
